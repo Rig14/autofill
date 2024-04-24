c420d7df50b2843e17fab37392285039dc7afb67
ririvi <ririvi@taltech.ee>
2024-04-24 22:17:14 +0300

    refactor: made code easier to read

--- main.py
+++ main.py
@@ -4,39 +4,29 @@ import string
 from api import generate_text
 
 GENERATE_TEXT_SHORTCUT = "ctrl+alt+space"
+TEXT_WRITING_SPEED = 0.03
 
 
 def get_text():
-    keys: list[keyboard.KeyboardEvent] = []
-    keyboard.hook(keys.append)
-    keyboard.wait(GENERATE_TEXT_SHORTCUT)
-    keyboard.unhook_all()
+    inputs = keyboard.record(GENERATE_TEXT_SHORTCUT)
+    generator = keyboard.get_typed_strings(inputs)
+    text = "".join([text for text in generator if text])
 
-    res = ""
-    for key in keys:
-        if key.event_type == keyboard.KEY_DOWN:
-            name = key.name
-            if name == "space":
-                res += " "
-                continue
-
-            if name not in string.ascii_letters:
-                continue
-            else:
-                res += name
-
-    return res
+    return text
 
 
 def type_text(text: str) -> None:
-    keyboard.write(text)
+    # clean text
+    text = "".join([x for x in text if x in string.ascii_letters + " "])
+    keyboard.write(text, delay=TEXT_WRITING_SPEED)
 
 
 def main():
     # listen for keyboard input and store it in a list
-    res = get_text()
-    generated_text = generate_text(res)
-    type_text(generated_text)
+    while True:
+        res = get_text()
+        generated_text = generate_text(res)
+        type_text(generated_text)
 
 
 if __name__ == "__main__":
