a00c6bcf4018f4d213bbd96f3b7e1f216f8d2b9d
ririvi <ririvi@taltech.ee>
2024-04-27 14:02:18 +0300

    feat: added clearing cache functionality, program is also less prone to stopping from errors

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import keyboard
 from api import generate_text
 
 GENERATE_TEXT_SHORTCUT = "ctrl+alt+space"
+CLEAR_CACHE_SHORTCUT = "ctrl+alt+c"
 TEXT_WRITING_SPEED = 0.03
+
+inputs: list[keyboard.KeyboardEvent] = []
 
 
 def get_text() -> str:
@@ -12,10 +15,10 @@ def get_text() -> str:
     Returns:
         str: The text that user has typed.
     """
-    inputs = keyboard.record(GENERATE_TEXT_SHORTCUT)
+    keyboard.wait(GENERATE_TEXT_SHORTCUT)
     generator = keyboard.get_typed_strings(inputs)
     text = "".join([text for text in generator if text])
-
+    print(text)
     return text
 
 
@@ -25,10 +28,18 @@ def type_text(text: str) -> None:
     Args:
         text (str): The text to type.
     """
-    keyboard.write(text, delay=TEXT_WRITING_SPEED)
+    try:
+        for line in text.split("\n"):
+            keyboard.write(line, delay=TEXT_WRITING_SPEED)
+            keyboard.press_and_release("enter")
+    except StopIteration:
+        print(text)
+        print("Text generation stopped.")
 
 
 def main():
+    keyboard.hook(inputs.append)
+    keyboard.add_hotkey(CLEAR_CACHE_SHORTCUT, lambda: inputs.clear())
     while True:
         res = get_text()
         generated_text = generate_text(res)
